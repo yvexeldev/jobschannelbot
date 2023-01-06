93eadb4411251157f4c693b06386f24d6fe41193
azam1307 <=>
2023-01-06 15:22:47 +0500

    DONE!

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+// console.log(process.env.BOT_TOKEN)
+
+export * from './core/index.js';
+export * from './actions/index.js';
+console.log(`BOT RUN`);
